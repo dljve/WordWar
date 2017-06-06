3f906299ea8933325952c9cce0ad8b18746e6a69
Douwe van Erp <dljvanerp@gmail.com>
2017-06-06 10:14:23 +0200

    Removed debug wordlist

--- core/src/com/applab/wordwar/server/RivialServer.java
+++ core/src/com/applab/wordwar/server/RivialServer.java
@@ -230,8 +230,6 @@ public class RivialServer implements Runnable{
         }
         System.out.println(condition);
 
-        filename = "DEBUGswahili-english.txt"; // TODO REMOVE!!!
-
         int port = 8888;
         System.out.println(port);
         try {
